51f199a44c06323fb8adb8fdba04df29c8871aeb
fulldev319 <fulldev326@gmail.com>
2020-02-03 20:51:00 -0800

    initial commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { toggleTodoAction, deleteTodoAction } from "../redux";
+
+export default () => {
+  const todos = useSelector(state => state.todos);
+  const dispatch = useDispatch();
+  const toggleTodo = todoId => dispatch(toggleTodoAction(todoId));
+  const deleteTodo = todoId => dispatch(deleteTodoAction(todoId));
+
+  return (
+    <ul className="todo-list">
+      {todos.map(todo => (
+        <li key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.complete}
+            onChange={toggleTodo.bind(null, todo.id)}
+          />
+          <span className={todo.complete ? "complete" : null}>{todo.name}</span>
+          <span
+            className="delete-button"
+            onClick={deleteTodo.bind(null, todo.id)}
+          >
+            x
+          </span>
+        </li>
+      ))}
+    </ul>
+  );
+};
